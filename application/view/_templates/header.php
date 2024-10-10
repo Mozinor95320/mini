949217482f4189d49e5e69aefd8932a77a65104c
Moz <43842362+Mozinor95320@users.noreply.github.com>
2024-10-10 18:52:39 +0000

    hhh

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8">
     <title>MINI</title>
@@ -11,18 +12,33 @@
     <!-- See more here: http://stackoverflow.com/q/2105327/1114320 -->
 
     <!-- CSS -->
+    <!-- Bootstrap CSS CDN -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="<?php echo URL; ?>css/style.css" rel="stylesheet">
-</head>
-<body>
-    <!-- logo -->
-    <div class="logo">
-        MINI
-    </div>
 
-    <!-- navigation -->
-    <div class="navigation">
-        <a href="<?php echo URL; ?>">home</a>
-        <a href="<?php echo URL; ?>home/exampleone">subpage</a>
-        <a href="<?php echo URL; ?>home/exampletwo">subpage 2</a>
-        <a href="<?php echo URL; ?>tracabiltySheets">tracabiltySheets</a>
-    </div>
+</head>
+
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <a class="navbar-brand" href="#">MyBrand</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active">
+                    <a class="nav-link" href="<?php echo URL; ?>">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo URL; ?>home/exampleone">subpage</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo URL; ?>home/exampletwo">subpage 2</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="<?php echo URL; ?>tracabiltySheets">tracabiltySheets</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
