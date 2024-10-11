279bbac1429023e6bd6af4c9434b204b42f6b35a
Moz <43842362+Mozinor95320@users.noreply.github.com>
2024-10-11 21:52:28 +0000

    kkk

--- application/model/model.php
+++ application/model/model.php
@@ -19,10 +19,10 @@ class Model
     {
         $sql = "SELECT serialNumber, partNumber, workOrder, sheetCreationDate, lastTimeEdit, statusSheetOperator, statusSheetQuality FROM tracabilitySheets LIMIT :limit OFFSET :offset";
         $query = $this->db->prepare($sql);
-        $query->bindValue(':limit', $limit, PDO::PARAM_INT);
-        $query->bindValue(':offset', $offset, PDO::PARAM_INT);
+        $query->bindValue(':limit', (int)$limit, PDO::PARAM_INT);
+        $query->bindValue(':offset', (int)$offset, PDO::PARAM_INT);
         // useful for debugging: you can see the SQL behind above construction by using:
-        echo '[ PDO DEBUG ]: ' . Helper::debugPDO($sql, $parameters);  exit();
+        // echo '[ PDO DEBUG ]: ' . Helper::debugPDO($sql, $parameters);  exit();
         $query->execute();
         return $query->fetchAll();
     }
