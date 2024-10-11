8062165b86d2229f72f54e6ed4bdc0a48653ae63
Moz <43842362+Mozinor95320@users.noreply.github.com>
2024-10-11 10:18:30 +0000

    jj

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -34,16 +34,16 @@
                     <a class="nav-link" href="<?php echo URL; ?>">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center" href="<?php echo URL; ?>home/exampleone">subpage</a>
+                    <a class="nav-link" href="<?php echo URL; ?>home/exampleone">subpage</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center" href="<?php echo URL; ?>home/exampletwo">subpage 2</a>
+                    <a class="nav-link" href="<?php echo URL; ?>home/exampletwo">subpage 2</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link d-flex align-items-center" href="<?php echo URL; ?>tracabiltySheets">tracabiltySheets</a>
+                    <a class="nav-link" href="<?php echo URL; ?>tracabiltySheets">tracabiltySheets</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <button class="btn btn-link nav-link dropdown-toggle d-flex align-items-center" id="bd-theme" type="button" aria-expanded="false" data-bs-toggle="dropdown" data-bs-display="static" aria-label="Toggle theme (light)">
+                    <button class="btn btn-link nav-link dropdown-toggle" id="bd-theme" type="button" aria-expanded="false" data-bs-toggle="dropdown" data-bs-display="static" aria-label="Toggle theme (light)">
                         <i class="bi bi-gear my-1 theme-icon-active"></i>
                         <span class="d-lg-none ms-2" id="bd-theme-text">Toggle theme</span>
                     </button>
