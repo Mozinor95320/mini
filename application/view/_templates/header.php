3d76cb264d64a1795ca26ecbd039caa5e558562c
Moz <43842362+Mozinor95320@users.noreply.github.com>
2024-10-11 10:14:49 +0000

    ggg

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -43,7 +43,7 @@
                     <a class="nav-link" href="<?php echo URL; ?>tracabiltySheets">tracabiltySheets</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <button class="btn btn-link nav-link py-2 px-0 px-lg-2 dropdown-toggle d-flex align-items-center" id="bd-theme" type="button" aria-expanded="false" data-bs-toggle="dropdown" data-bs-display="static" aria-label="Toggle theme (light)">
+                    <button class="btn btn-link nav-link dropdown-toggle d-flex align-items-center" id="bd-theme" type="button" aria-expanded="false" data-bs-toggle="dropdown" data-bs-display="static" aria-label="Toggle theme (light)">
                         <i class="bi bi-gear my-1 theme-icon-active"></i>
                         <span class="d-lg-none ms-2" id="bd-theme-text">Toggle theme</span>
                     </button>
