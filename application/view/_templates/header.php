107e14a637b0656bf9d6e068c5308171f50b1122
Moz <43842362+Mozinor95320@users.noreply.github.com>
2024-10-12 16:34:09 +0000

    dd

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -21,14 +21,14 @@
 
 <body>
     <!-- Navbar -->
-    <nav class="navbar sticky-top navbar-expand-lg">
+    <nav class="navbar sticky-top navbar-expand-lg navbar-dark">
         <a class="navbar-brand" href="<?php echo URL . 'tracabiltySheets'?>">
             <img src="<?php echo URL; ?>img/logo_parker_crop.png" alt="Parker" height="24">
         </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse navbar-dark" id="navbarNav">
+        <div class="collapse navbar-collapse" id="navbarNav">
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item active">
                     <a class="nav-link" href="<?php echo URL . 'tracabiltySheets'?>">Home</a>
