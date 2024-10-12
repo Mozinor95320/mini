1ede0c5b67e6bcceff9be3b79134098aa359d101
Moz <43842362+Mozinor95320@users.noreply.github.com>
2024-10-12 13:47:48 +0000

    jj

--- application/view/_templates/header.php
+++ application/view/_templates/header.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <title>MINI</title>
     <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- JS -->
     <!-- please note: The JavaScript files are loaded in the footer to speed up page construction -->
