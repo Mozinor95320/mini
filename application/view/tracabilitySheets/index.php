fa1d9e6ff9b1500278a7201171f56bdebd5cfcd5
Moz <43842362+Mozinor95320@users.noreply.github.com>
2024-10-13 15:06:09 +0000

    dd

--- application/view/tracabilitySheets/index.php
+++ application/view/tracabilitySheets/index.php
@@ -47,6 +47,47 @@
                     </select>
                 </form>
 
+                <!--Filter by -->
+
+                <form action="<?php echo URL . 'tracabilitySheets/index/' ?>" method="POST">
+                    <label for="limitListTracabilitySheet">Filtré par : </label>
+                    <select name="limitListTracabilitySheet" id="limitListTracabilitySheet" onchange="this.form.submit()">
+                        <option value="10" <?php if ($limit == 10) echo 'selected'; ?>>10</option>
+                        <option value="20" <?php if ($limit == 20) echo 'selected'; ?>>20</option>
+                        <option value="50" <?php if ($limit == 50) echo 'selected'; ?>>50</option>
+                        <option value="100" <?php if ($limit == 100) echo 'selected'; ?>>100</option>
+                    </select>
+                </form>
+
+                <!--Var display sort -->
+
+                <form action="<?php echo URL . 'tracabilitySheets/index/' ?>" method="POST">
+                    <label for="limitListTracabilitySheet">Fiches par page : </label>
+                    <select name="limitListTracabilitySheet" id="limitListTracabilitySheet" onchange="this.form.submit()">
+                        <option value="10" <?php if ($limit == 10) echo 'selected'; ?>>10</option>
+                        <option value="20" <?php if ($limit == 20) echo 'selected'; ?>>20</option>
+                        <option value="50" <?php if ($limit == 50) echo 'selected'; ?>>50</option>
+                        <option value="100" <?php if ($limit == 100) echo 'selected'; ?>>100</option>
+                    </select>
+                </form>
+                <!--ACC OR DESC -->
+                <form action="<?php echo URL . 'tracabiltySheets/index/' ?>" method="POST">
+                    <div class="btn-group">
+                        <button type="button" class="btn btn-outline-primary">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sort-down" viewBox="0 0 16 16">
+                                <path d="M3.5 2.5a.5.5 0 0 0-1 0v8.793l-1.146-1.147a.5.5 0 0 0-.708.708l2 1.999.007.007a.497.497 0 0 0 .7-.006l2-2a.5.5 0 0 0-.707-.708L3.5 11.293zm3.5 1a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5M7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1z"></path>
+                            </svg>
+                            <span class="visually-hidden">Button</span>
+                        </button>
+                        <button type="button" class="btn btn-outline-primary">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-sort-up" viewBox="0 0 16 16">
+                                <path d="M3.5 12.5a.5.5 0 0 1-1 0V3.707L1.354 4.854a.5.5 0 1 1-.708-.708l2-1.999.007-.007a.5.5 0 0 1 .7.006l2 2a.5.5 0 1 1-.707.708L3.5 3.707zm3.5-9a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5M7.5 6a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1zm0 3a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1z"></path>
+                            </svg>
+                            <span class="visually-hidden">Button</span>
+                        </button>
+                    </div>
+                </form>
+
             </div>
         </div>
     </div>
