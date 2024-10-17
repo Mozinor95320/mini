53670c6598caa74aef27ec6a8bb7918281b88347
Moz <43842362+Mozinor95320@users.noreply.github.com>
2024-10-17 18:17:17 +0200

    y(y

--- application/view/tracabilitySheets/edit.php
+++ application/view/tracabilitySheets/edit.php
@@ -1347,5 +1347,5 @@ var serialNumberSheet = "<?php echo $tracabilitySheet->serialNumber; ?>";
 <!-- Adapter pour Chart.js -->
 <script src="https://cdn.jsdelivr.net/npm/chartjs-adapter-moment@1.0.0"></script>
 <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-zoom"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/hammer.js/2.0.8/hammer.min.js"></script>
 <script src="<?php echo URL; ?>js/windingCharts.js"></script>
+<script src="<?php echo URL; ?>js/tolerances.js"></script>
